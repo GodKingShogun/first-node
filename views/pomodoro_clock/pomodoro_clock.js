c6e20aea61f9481a9879c6b1a17095e6ace0e8d0
David Amazing <David Amazing>
2017-05-16 23:44:46 -0400

    update color scheme and font-family

--- views/pomodoro_clock/pomodoro_clock.js
+++ views/pomodoro_clock/pomodoro_clock.js
@@ -1,35 +1,40 @@
 $(document).ready(function(){
 var breakLength=10.00;
-var workLength=20.00
+var workLength=20.00;
 var breakCounter=breakLength;
 var workCounter=workLength;
 var isTicking=false;
 var breakTime= false;
 var interval=null;
- if (isTicking=false){
+
+ if (isTicking===false){
 $("#addB").on("click",function(){
 breakLength=breakLength+1.00;
 breakCounter=breakCounter+1;
 $("#break-well").html(breakCounter);
-});}
-  if (isTicking=false){
+});
+}
+  if (isTicking===false){
 $("#subtractB").on("click",function(){
 breakLength=breakLength-1.00;
 breakCounter=breakCounter-1;
 $("#break-well").html(breakCounter);
-});}
-  if (isTicking=false){
+});
+}
+  if (isTicking===false){
 $("#addW").on("click",function(){
 workLength=workLength+1.00;
 workCounter=workCounter+1;
 $("#work-well").html(workCounter);
-});}
-  if (isTicking=false){
+});
+}
+  if (isTicking===false){
 $("#subtractW").on("click",function(){
  workLength=workLength-1;
  workCounter= workCounter-1;
 $("#work-well").html(workCounter);
-});}
+});
+}
 
 $("#breakSS").on("click",function(){
   if (!isTicking) {
